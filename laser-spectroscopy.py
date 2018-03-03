7fbefdb43757a869e68317f7757d661a123dc26c
Josh Porter <jccporter@gmail.com>
2018-03-03 23:33:35 +0000

    Improved MultiPlot

--- laser-spectroscopy.py
+++ laser-spectroscopy.py
@@ -6,7 +6,6 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import uncertainties
 from lmfit.models import PseudoVoigtModel, LinearModel
 from scipy.optimize import curve_fit
 
@@ -44,6 +43,7 @@ class RangeTool:
         self.cid2 = figure2.figure.canvas.mpl_connect('motion_notify_event', self.mouse_move)
         self.cid3 = figure2.figure.canvas.mpl_connect('key_press_event', self.rangeremove)
         self.cid4 = figure2.figure.canvas.mpl_connect('key_press_event', self.finishplot)
+        self.cid5 = figure2.figure.canvas.mpl_connect('pick_event', self.pick_handler)
         self.Ranges = pd.DataFrame(columns=['Lower Bound', 'LowerIndex', 'Upper Bound', 'UpperIndex', 'Displayed'])
         self.il = 0.
         self.iu = 0.
@@ -54,6 +54,13 @@ class RangeTool:
         print(event.xdata, event.ydata)
         if event.inaxes != self.figure2.axes:
             return
+
+    # Currently unused, the end goal of this method is to make the legend interactive so that specific range selections
+    # can be removed as supposed to only being able to remove the most recent one
+    def pick_handler(self, event):
+        mouseevent = event.mouseevent
+        artist = event.artist
+        print(artist)
 
     def mouse_move(self, event):
 
@@ -72,6 +79,9 @@ class RangeTool:
         self.figure2.figure.canvas.draw_idle()
         # print('x=%1.2f, y=%1.2f' % (x, y))
 
+    # Selects the ranges to be used for fitting, note the il and iu variables, these are used as a way of preventing
+    # range selections that don't contain both a lower and upper bound from being added to the list. This check is
+    # done in line 100
     def rangeselect(self, event):
         if not event.inaxes:
             return
@@ -118,12 +128,15 @@ class RangeTool:
                     polys[0].remove()
                     if self.Ranges == 'Empty DataFrame':
                         print('Range list is empty')
-                # except NotImplementedError:
-                #     polys[len(self.Ranges.index)] = polys(alpha=0)[len(self.Ranges.index)]
                 finally:
                     pass
+                # The rangeselect function is called at the end of the rangeremoving tool so that further ranges can
+                # be added once one is deleted. There is probably a better way of doing it, but this worked.
                 self.cid1 = self.figure2.figure.canvas.mpl_connect('key_press_event', self.rangeselect)
 
+    # Allows the plot to be closed such that new ranges are/aren't written. The main case is when you want to look at a
+    # plot but not change the ranges - closing with enter will write whatever ranges are currently on screen, closing
+    # with escape won't write any files.
     def finishplot(self, event):
         if event.key == 'enter':
             os.chdir('C:\\Users\Josh\Desktop\LSPEC1\Ranges')
@@ -140,6 +153,7 @@ class RangeTool:
 Data = {}
 
 
+# Reads the converted datafiles
 class DataRead:
     def __init__(self, exp, peak, run, end):
         self.IndependentVariable = "Frequency (a.u)"
@@ -176,6 +190,7 @@ class DataRead:
             self.yranges[o] = self.yrange[o]
         return self.xranges, self.yranges, self.xrange, self.yrange
 
+    # Uses lmfit to fit a pseudovoigt to the ranges that are found in csvs
     def singleplot(self):
         start_time = time.time()
 
@@ -225,33 +240,40 @@ class DataRead:
             # print(self.xranges[i])
             voigt_mod = PseudoVoigtModel(prefic='voigt_')
             line_mod = LinearModel(prefix='line_')
-            params = line_mod.make_params(intercept=np.min(self.yrange[i]))
-            params += voigt_mod.guess(self.yranges[i], x=self.xranges[i], center=np.median(self.xranges[i]),
-                                      sigma=np.std(self.xranges[i]), height=np.max(self.yranges[i]), fraction=0.7)
+            params = line_mod.guess(self.yranges[i], x=self.xranges[i])
+            params += voigt_mod.guess(self.yranges[i], x=self.xranges[i])
+            params.add('center', value=np.median(self.xranges[i]),
+                       min=np.min(self.xranges[i]) * 0.9,
+                       max=np.max(self.xranges[i]) * 1.1)
+            params.add('fraction', value=0.95, max=1.9)
+
+            mate = np.abs(self.yranges[i]) ** (1 / 2)
             model = voigt_mod + line_mod
-            # params['gamma'].set(value=np.std(self.xranges[i]), vary=True)
-            result = model.fit(self.yranges[i], params, x=self.xranges[i])
-            labels = dictdict[self.peak]
-            plt.plot(self.xranges[i], result.best_fit, antialiased=True,
-                     label='\n' + labels[i] + '\n${0:.2f} MHz$'.format(result.params['center'].value))
+            result = model.fit(self.yranges[i], params, x=self.xranges[i], weights=mate)
+            # labels = dictdict[self.peak]
+            # plt.plot(self.xranges[i], result.best_fit, antialiased=True,
+            #          label='\n' + labels[i] + '\n${0:.2f} MHz$'.format(result.params['center'].value))
+            plt.plot(self.xranges[i], result.best_fit,
+                     label='${0:.2f} MHz$'.format(result.params['center'].value), antialiased=True)
             # dely = result.eval_uncertainty(sigma=1)
             # plt.fill_between(self.xranges[i], result.best_fit - dely, result.best_fit + dely, color="#ABABAB")
             plt.legend(fancybox=True, mode='expand', handlelength=0.01)
-            centers = uncertainties.ufloat(result.params['center'].value, result.params['center'].stderr)
-            fwhms = uncertainties.ufloat(result.params['fwhm'].value, result.params['fwhm'].stderr)
-            fractions = uncertainties.ufloat(result.params['fraction'].value, result.params['fraction'].stderr)
-            peakdict = {1: '87b', 2: '85b', 3: '85a', 4: '87a'}
-            self.fitvals = self.fitvals.append({
-                                      'Hyperfine transition': labels[i],
-                                      'Relative frequency (MHz)': centers,
-                                      'FWHM (MHz)': fwhms,
-                                      'Fraction': fractions}, ignore_index=True)
+            # centers = uncertainties.ufloat(result.params['center'].value, result.params['center'].stderr)
+            # fwhms = uncertainties.ufloat(result.params['fwhm'].value, result.params['fwhm'].stderr)
+            # fractions = uncertainties.ufloat(result.params['fraction'].value, result.params['fraction'].stderr)
+            # peakdict = {1: '87b', 2: '85b', 3: '85a', 4: '87a'}
+            # self.fitvals = self.fitvals.append({
+            #                           'Hyperfine transition': labels[i],
+            #                           'Relative frequency (MHz)': centers,
+            #                           'FWHM (MHz)': fwhms,
+            #                           'Fraction': fractions}, ignore_index=True)
             # self.texwriter()
             # print(labels[i])
-            # print(result.fit_report())
+            print(result.fit_report())
             # print('----{}----'.format(time.time() - start_time))
         # fitvals.to_csv('fit_{}_{}.csv'.format(self.peak, self.run))
 
+    # Writes a .tex file that when run produces a table of important values
     def texwriter(self):
         os.chdir('C:\\Users\Josh\Desktop\LSPEC1\LATEX')
         with open('{}_{}_{}_{}.tex'.format(self.exp, self.peak, self.run, self.end), 'w') as texfile:
@@ -291,19 +313,92 @@ class DataRead:
             texfile.write('\\end{document}\n')
             os.chdir('C:\\Users\Josh\Desktop\LSPEC1')
 
+    # Function for plotting residuals etc.
     def multiplot(self):
+        a_87 = {0: "$F=2 \Rightarrow F'=3$",
+                1: "Unknown trough",
+                2: "Crossover: $F'=2, F'=3$",
+                3: "Crossover: $F'=1, F'=3$",
+                4: "$F=2 \Rightarrow F'=2$",
+                5: "Crossover: $F'=2, F'=1$",
+                6: "$F=2 \Rightarrow F'=1$"}
+        a_85 = {0: "$F=3 \Rightarrow F'=4$",
+                1: "Crossover: $F'=3, F'=4$",
+                2: "Crossover: $F'=2, F'=4$",
+                3: "$F=3 \Rightarrow F'=3$",
+                4: "Crossover: $F'=3, F'=2$",
+                5: "$F=3 \Rightarrow F'=2$"}
+        b_87 = {0: "$F=1 \Rightarrow F'=2$",
+                1: "Crossover: $F'=1, F'=2$",
+                2: "Crossover: $F'=0, F'=2$",
+                3: "$F=1 \Rightarrow F'=1$",
+                4: "Crossover: $F'=1, F'=0$",
+                5: "$F=1 \Rightarrow F'=0$"}
+        b_85 = {0: "$F=2 \Rightarrow F'=3$",
+                1: "Crossover: $F'=2, F'=3$",
+                2: "Crossover: $F'=1, F'=3$",
+                3: "$F=2 \Rightarrow F'=2$",
+                4: "Crossover: $F'=1, F'=2$",
+                5: "$F=2 \Rightarrow F'=1$"}
+        a_872 = {0: "F2_Fp3",
+                 1: "Unknown_trough",
+                 2: "CO_Fp2_Fp3",
+                 3: "CO_Fp1_Fp3",
+                 4: "F2_Fp2",
+                 5: "CO_Fp2_Fp1",
+                 6: "F2_Fp1"}
+        a_852 = {0: "F3_Fp4",
+                 1: "CO_Fp3_Fp4",
+                 2: "CO_Fp2_Fp4",
+                 3: "F3_Fp3",
+                 4: "CO_Fp3_Fp2",
+                 5: "F3_Fp2"}
+        b_872 = {0: "F1_Fp2",
+                 1: "CO_Fp1_Fp2",
+                 2: "CO_Fp0_Fp2",
+                 3: "F1_Fp1",
+                 4: "CO_Fp1_Fp0",
+                 5: "F1_Fp0"}
+        b_852 = {0: "F2_Fp3",
+                 1: "CO_Fp2_Fp3",
+                 2: "CO_Fp1_Fp3",
+                 3: "F2_Fp2",
+                 4: "CO_Fp1_Fp2",
+                 5: "F2_Fp1"}
+
+        dictdict = {1: b_87, 2: b_85, 3: a_85, 4: a_87}
+        dictdict3 = {1: b_872, 2: b_852, 3: a_852, 4: a_872}
+
+        dictdict2 = {1: 'Rb-87', 2: 'Rb-85', 3: 'Rb-85', 4: 'Rb-87'}
+        labels = dictdict[self.peak]
         os.chdir('C:\\Users\Josh\Desktop\LSPEC1\{}'.format(self.rangepath))
         self.rangename = pd.read_csv('{}.csv'.format(self.datafilename), engine='c')
         self.range()
         for i in range(0, len(self.xrange)):
             voigt_mod = PseudoVoigtModel(prefic='voigt_')
             line_mod = LinearModel(prefix='line_')
-            params = line_mod.make_params(intercept=np.min(self.yrange[i]))
-            params += voigt_mod.guess(self.yranges[i], x=self.xranges[i], center=np.median(self.xranges[i]),
-                                      sigma=np.std(self.xranges[i]), height=np.max(self.yranges[i]), fraction=0.7)
+            params = line_mod.guess(self.yranges[i], x=self.xranges[i])
+            params += voigt_mod.guess(self.yranges[i], x=self.xranges[i])
+            params.add('center', value=np.median(self.xranges[i]),
+                       min=np.min(self.xranges[i]) * 0.85,
+                       max=np.max(self.xranges[i]) * 1.15)
+            # params.add('fraction', value=0.7, max=1.9)
             model = voigt_mod + line_mod
-            result = model.fit(self.yranges[i], params, x=self.xranges[i])
+            mate = np.abs(self.yranges[i]) ** (1 / 2)
+            result = model.fit(self.yranges[i], params, x=self.xranges[i], weights=mate)
 
+            plt.rc('text', usetex=True)
+            plt.rc('font', family='serif')
+            plt.rcParams['text.latex.preamble'] = [
+                r'\usepackage{siunitx}',  # i need upright \micro symbols, but you need...
+                r'\sisetup{detect-all}',  # ...this to force siunitx to actually use your fonts
+                r'\usepackage{lmodern}',  # set the normal font here
+            ]
+            plt.rcParams['ps.usedistiller'] = 'xpdf'
+            plt.rcParams['ps.distiller.res'] = '16000'
+
+            size = 12
+            size2 = 16
             fig = plt.figure()
             fig.subplots_adjust(hspace=0.3, wspace=0)
             ax1 = fig.add_subplot(2, 2, 1)
@@ -311,38 +406,60 @@ class DataRead:
             ax1.plot(self.xranges[i], self.yranges[i], '.', color='#1c1c1c')
             dely = result.eval_uncertainty(sigma=1)
             ax1.fill_between(self.xranges[i], result.best_fit - dely, result.best_fit + dely, color="#ABABAB")
-            ax1.grid(color='k', linestyle='--', alpha=0.2)
-            plt.title('Peak with 1 sigma error bands')
+            ax1.grid(color='k', linestyle='--', alpha=0.2, antialiased=True)
+            plt.title(r'$\textit{Peak with 1 sigma error bands}$', fontsize=size2)
+            plt.xlabel(r'$\textit{Frequency} \textit{ (MHz+C)}$', fontsize=size)
+            plt.ylabel(r'$\textit{Intensity (V)}$', fontsize=size)
 
             ax2 = fig.add_subplot(2, 2, 2)
-            ax2.plot(self.xranges[i], self.yranges[i]-result.best_fit, '.', antialiased=True)
+            ax2.plot(self.xranges[i], self.yranges[i] - result.best_fit, '.', antialiased=True, color='#1c1c1c')
             ax2.grid(color='k', linestyle='--', alpha=0.2)
-            plt.title('Residuals')
+            plt.title(r'$Residuals$', fontsize=size2)
+            plt.ylabel(r'$f_m - f_f \textit{ (MHz+C)}$', fontsize=size)
+            plt.xlabel(r'$\textit{Frequency} \textit{ (MHz+C)}$', fontsize=size)
 
             ax3 = fig.add_subplot(2, 2, 3)
-            ax3.plot(self.xranges[i], ((self.yranges[i]-result.best_fit)**2)/(dely**2), '.', antialiased=True)
+            ax3.plot(self.xranges[i], ((self.yranges[i] - result.best_fit) ** 2) / (dely ** 2), '.', antialiased=True,
+                     color='#1c1c1c')
             ax3.grid(color='k', linestyle='--', alpha=0.2)
-            plt.title('Normalised residuals')
+            plt.title(r'$\textit{Normalised residuals}$', fontsize=size2)
+            plt.xlabel(r'$\textit{Frequency} \textit{ (MHz+C)}$', fontsize=size)
+            plt.ylabel(r'$(f_m - f_f)^2 \div \sigma^2$', fontsize=size)
 
             ax4 = fig.add_subplot(2, 2, 4)
-            ax4.hist(self.yranges[i]-result.best_fit, bins=15)
+            ax4.hist(self.yranges[i] - result.best_fit, bins=30, antialiased=True, color='#1c1c1c')
             ax4.grid(color='k', linestyle='--', alpha=0.2)
-            plt.title('Residual histogram')
+            plt.xlabel(r'$f_m - f_f \textit{ (MHz+C)}$', fontsize=size)
+            plt.ylabel(r'$\textit{Counts}$', fontsize=size)
+            plt.title(r'$\textit{Residual histogram}$', fontsize=size2)
+
 
             fig.tight_layout()
             fig.set_size_inches(16.5, 10.5)
             fig_manager = plt.get_current_fig_manager()
             fig_manager.window.showMaximized()
-            fig.suptitle(('Peak {0:.0f}'.format(self.peak) + ' ' +
-                          'Run {0:.0f}'.format(self.run) + ' ' +
-                          'Hyperfine peak at {0:.5f}'.format(result.params['center'].value)))
+            fig.suptitle(('{} {}'.format(dictdict2[self.peak], labels[i]) + ' ' +
+                          'at {0:.4f} MHz'.format(result.params['center'].value)), fontsize=18)
+            plt.subplots_adjust(top=0.884,
+                                bottom=0.084,
+                                left=0.077,
+                                right=0.956,
+                                hspace=0.555,
+                                wspace=0.247)
             print(self.peak, self.run, result.params['center'].value)
             print('Peak {0:2d}, Run {0:.0f}, Hyperfine peak at {0:.5f}'.format(self.peak, self.run,
                                                                                result.params['center'].value))
-            plt.show()
+            os.chdir('C:\\Users\Josh\Desktop\LSPEC1\Figures')
+            plt.savefig(
+                '{}_{}_{}_{}_{}resid.png'.format(self.exp, self.peak, self.run, self.end, dictdict3[self.peak][i]),
+                dpi=600)
+            os.chdir('C:\\Users\Josh\Desktop\LSPEC1\Ranges')
+            # plt.show()
             print(result.fit_report())
 
 
+# Takes oscilloscope csv file that includes columns stating things such as x and y deviations and writes a new csv
+# that has two headerless columns
 def DataConvert(datafolder, destinationfolder):
     os.chdir('C:\\Users\Josh\Desktop\LSPEC1\{}'.format(datafolder))
     for filename in os.listdir(os.getcwd()):
@@ -377,28 +494,31 @@ def doot(exp, peak, run, end):
     fig, ax = plt.subplots()
     figure2, = ax.plot(DataRead(exp, peak, run, end).dataset[DataRead(exp, peak, run, end).IndependentVariable],
                        DataRead(exp, peak, run, end).dataset[DataRead(exp, peak, run, end).DependentVariable],
-                       '.', antialiased='True', color='#1c1c1c', mew=1.0, markersize=2.5)
-    thing = RangeTool(ax, DataRead(exp, peak, run, end).dataset, DataRead(exp, peak, run, end).datafilename, figure2)
+                       '.', antialiased='True', color='#1c1c1c', mew=1.0, markersize=2.5, picker=5)
+    # thing = RangeTool(ax, DataRead(exp, peak, run, end).dataset, DataRead(exp, peak, run, end).datafilename, figure2)
     plt.ylabel('Intensity (a.u)')
     plt.xlabel('Frequency (a.u)')
-    plt.title('{} {}'.format(peakdict[peak], end))
+    # plt.title('{} {}'.format(peakdict[peak], end))
+    plt.title('{} {} {} {}'.format(exp, peak, run, end))
     ax.grid(color='k', linestyle='--', alpha=0.2)
     fig.set_size_inches(13.5, 10.5)
 
     print('\n')
     if os.path.isfile('C:\\Users\Josh\Desktop\LSPEC1\Ranges\{}_{}_{}_{}.csv'.format(exp, peak, run, end)):
-        DataRead(exp, peak, run, end).singleplot()
+        DataRead(exp, peak, run, end).multiplot()
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.legend(fancybox=True, loc='center left', bbox_to_anchor=(1, 0.5))
     os.chdir('C:\\Users\Josh\Desktop\LSPEC1\Figures')
-    # plt.savefig('{}_{}_{}_{}.png'.format(exp, peak, run, end), dpi=600)
+    # plt.savefig('{}_{}_{}_{}resid.png'.format(exp, peak, run, end), dpi=600)
     os.chdir('C:\\Users\Josh\Desktop\LSPEC1')
     figManager = plt.get_current_fig_manager()
     figManager.window.showMaximized()
-    plt.show()
+    # print(plt.gcf().get_children())
+    # plt.show()
 
 
+# multidoot is currently useless and may be removed
 def multidoot():
     fig, axes = plt.subplots(nrows=2, ncols=2)
 
@@ -479,9 +599,9 @@ def calib():
     os.chdir('C:\\Users\Josh\Desktop\LSPEC1\ReadableData')
 
 
-for j in {'R0', 'R5', 'R10', 'R15', 'R25', 'R35', 'R45'}:
-    for i in range(1, 5):
-        doot('SAT', i, 6, j)
+for j2 in {'R5', 'R10', 'R15', 'R25', 'R35', 'R45'}:
+    for i2 in range(1, 5):
+        doot('SAT', i2, 6, j2)
 # DataConvert('Data', 'ReadableData')
-
+# doot('ZEE', 5, 1, 'RDAT')
 print('Complete')
